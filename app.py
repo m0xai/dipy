821485d480bc51edccf09cad98cd191d5d0d5ab3
Kerem Zopcuk <keremzopcuk@gmail.com>
2024-02-23 20:51:53 +0100

    feat: respond with error, if bot commands fails

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
 import asyncio
 import os
+import sys
+import traceback
 
 import discord
 from discord.ext import commands
@@ -22,5 +24,24 @@ async def main():
         await bot.load_extension("cmds.ig_dl")
         await bot.start(app_token)
 
+
+async def on_command_error(ctx: commands.Context, error):
+    # Handle your errors here
+    if isinstance(error, commands.MemberNotFound):
+        await ctx.send("I could not find member '{error.argument}'. Please try again")
+
+    elif isinstance(error, commands.MissingRequiredArgument):
+        await ctx.send(f"'{error.param.name}' is a required argument.")
+    elif isinstance(error, commands.CommandInvokeError):
+        await ctx.send(str(error))
+    else:
+        # All unhandled errors will print their original traceback
+        print(f"Ignoring exception in command {ctx.command}:", file=sys.stderr)
+        traceback.print_exception(
+            type(error), error, error.__traceback__, file=sys.stderr
+        )
+
+
+bot.on_command_error = on_command_error
 
 asyncio.run(main())
